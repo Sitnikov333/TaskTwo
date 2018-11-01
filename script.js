5458cee3461ca157326f73a8b9264fe2d6fb90a5
Sitnikov333 <44017329+Sitnikov333@users.noreply.github.com>
2018-11-01 17:33:30 +0500

    100% end vers
    sdelal)

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// $(document).ready(function(){
-//     $('.spoiler_links').click(function(){
-//         $(this).parent().children('div.spoiler_body').toggle('normal');
-//         return false;
-//     });
-// });
-
 
 $(function() {
     var block = $('.block');
@@ -33,43 +26,6 @@ function swap(a, b) {
         a.insertBefore(b);
     }
 }
-
-// jQuery.fn.swap = function(b){
-//     // method from: http://blog.pengoworks.com/index.cfm/2008/9/24/A-quick-and-dirty-swap-method-for-jQuery
-//     b = jQuery(b)[0];
-//     var a = this[0];
-//     var t = a.parentNode.insertBefore(document.createTextNode(''), a);
-//     b.parentNode.insertBefore(a, b);
-//     t.parentNode.insertBefore(b, t);
-//     t.parentNode.removeChild(t);
-//     return this;
-// };
-//
-//
-// $( ".block" ).draggable({ revert: true, helper: "clone" });
-//
-// $( ".block" ).droppable({
-//     accept: ".block",
-//     activeClass: "ui-state-hover",
-//     hoverClass: "ui-state-active",
-//     drop: function( event, ui ) {
-//
-//         var draggable = ui.draggable, droppable = $(this),
-//             dragPos = draggable.position(), dropPos = droppable.position();
-//
-//         draggable.css({
-//             left: dropPos.left+'px',
-//             top: dropPos.top+'px'
-//         });
-//
-//         droppable.css({
-//             left: dragPos.left+'px',
-//             top: dragPos.top+'px'
-//         });
-//         draggable.swap(droppable);
-//     }
-// });
-//
 
 $(document).ready(function(){
     $('.spoiler_links').click(function(){
